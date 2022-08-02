09ea9a9b8c161b769ed1ecb6f17b6450d3518600
MRW Neundorf <ninevillage@gmail.com>
2022-08-02 15:03:18 +0200

    chore(site): updated to next vuepress (2.x)
    Added google header tag.
    Updated google analytics to next.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,17 +1,34 @@
+const { googleAnalyticsPlugin } = require('@vuepress/plugin-google-analytics')
+
 module.exports = {
   title: 'naxmefy',
   description: 'just a dev',
   baseUrl: 'https://nax.me/',
   extraWatchFiles: ['**/*.md', '**/*.vue'],
-  plugins: {
-    sitemap: {
-      hostname: 'https://nax.me'
-    },
-    robots: {
-      host: 'https://nax.me'
-    },
-    '@vuepress/google-analytics': {
-      ga: 'UA-27498828-5'
-    }
-  }
+  head: [
+    [
+      'meta',
+      {
+        name: 'google-site-verification',
+        content: '3SK_eEbXBonIE1ioei8UUIrdV0NWNTyFgrL_6dmMqIA'
+      }
+    ]
+  ],
+  plugins: [
+    [
+      'sitemap',
+      {
+        hostname: 'https://nax.me'
+      }
+    ],
+    [
+      'robots',
+      {
+        host: 'https://nax.me'
+      }
+    ],
+    googleAnalyticsPlugin({
+      id: 'G-325582735'
+    })
+  ]
 }
